4e50c945d10d58be93f4f59d19a2e19ee0673a72
EdFrost <rn.nagelhout@gmail.com>
2015-09-17 10:46:14 +0200

    first

---
+++ countdown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class countdowntimer : MonoBehaviour {
+	int minutes;
+	int seconds;
+	string niceTime;
+	public static bool timeStarted = true;
+	 public float timeLeft = 600.0f;
+	
+	void Update()
+	{
+		if (timeStarted == true) {
+			timeLeft -= Time.deltaTime;
+			}
+		if(timeLeft < 0)
+		{
+			Application.LoadLevel("MPvictory");
+		}
+	}
+	void OnGUI() {
+		minutes = Mathf.FloorToInt(timeLeft / 60F);
+		seconds = Mathf.FloorToInt(timeLeft - minutes * 60);
+		
+		niceTime = string.Format("{00:00}:{01:00}", minutes, seconds);
+		
+		GUI.Label(new Rect(10,10,250,100), niceTime);
+	}
+}
---
+++ deathcountSP.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathcountSP : MonoBehaviour {
+	public GameObject player1;
+	float deathtimeout = 0;
+
+
+	int deathCounter;
+	void Start(){
+
+		}
+	void FixedUpdate(){
+		deathtimeout -= Time.deltaTime;
+
+		Health eh = (Health)player1.GetComponent("Health");
+
+//		Debug.Log (eh.currentHealth);
+		if(eh.currentHealth <= 0 && deathtimeout <= 0){
+			deathCounter++;
+			Debug.Log("deaths:" + deathCounter);
+			deathtimeout = 8.0f;
+		}
+	}
+	
+	void OnGUI(){
+		//Draws out a box on position x and y, with a specified width and height and width.
+		GUI.Label(new Rect(5, 40, 120, 20),"Deaths:" + deathCounter);
+	}
+}
